8fa15102206654766ad01c26d150d2d936a81b79
adscud <34245352+adscud@users.noreply.github.com>
2023-01-05 11:13:30 +0100

    setup flashlist bug

--- App.tsx
+++ App.tsx
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import React from "react";
+import { FlashList } from '@shopify/flash-list';
+
+const { height, width } = Dimensions.get('window')
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 export default function App() {
+  const renderItem = ({ index }: { index: number }) => {
+    return <ListItem index={index} />
+  }
+
+  const keyExtractor = (item: any, index: number) => index.toString()
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlashList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        estimatedItemSize={height}
+        estimatedListSize={{height, width}}
+        pagingEnabled
+      />
     </View>
   );
+}
+
+function ListItem({index}: {index: number}) {
+  // This square is supposed to be present only on the first item
+  const [showSquare] = React.useState(index === 0);
+
+  return (
+    <View style={[styles.item, {height, width, backgroundColor: index % 2 === 0 ? 'red' : 'yellow'}]}>
+      {showSquare && <View style={styles.square} />}
+      <Text style={[styles.index, {color: index % 2 === 0 ? 'yellow' : 'red'}]}>
+        {index.toString()}
+      </Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  item: {
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  index: {
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  square: {
+    position: 'absolute',
+    top: 50,
+    left: 0,
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue'
+  }
 });
